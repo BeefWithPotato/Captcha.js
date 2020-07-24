be72f35ec7226999c7c443a1a59d902b501203c4
Haosheng Zhang <haoshengg.zhang@mail.utoronto.ca>
2020-07-24 18:34:07 -0400

    add reset function

--- public/js/examples.js
+++ public/js/examples.js
@@ -2,7 +2,6 @@
 const log = console.log
 
 const uploadedImg = [];
-
 const ex1 = new SlideStyleGenerator();
 ex1.imgs = uploadedImg;
 const uploadEx = document.querySelector('#by-user');
@@ -14,15 +13,20 @@ uploadEx.append(block);
 
 const defaultImg = [];
 setDefaultImgs();
-
 const ex2 = new SlideStyleGenerator();
 ex2.srcs = defaultImg;
 const defaultEx = document.querySelector('#by-developer');
 const canvas2 = ex2.createCanvas(320, 200, "canvas2", "canvas");
 const block2 = ex2.createCanvas(320, 200, "part2", "part");
+
 defaultEx.append(canvas2);
 defaultEx.append(block2);
 ex2.makeJigsaw(0, 0, 320, 200, 50, "src");
+// const button = document.createElement("button");
+// button.type = "button";
+// button.onclick = resetDefault();
+// button.append(document.createTextNode("Reset"));
+// defaultEx.append(button);
 
 
 function saveImages(){
@@ -33,14 +37,14 @@ function saveImages(){
 		   uploadedImg.push(file);
 		});
 		
-
+		canvas.height = canvas.height;
+		block.height = block.height;
+		// const canvasContext = canvas.getContext("2d");
+		// canvasContext.clearRect(0,0,320, 200);
+		// const blockContext = block.getContext("2d");
+		// blockContext.clearRect(0,0,320, 200);
 		ex1.pastX = 0;
 		ex1.makeJigsaw(0, 0, 320, 200, 50, "file");
-
-		const h4 = document.createElement("h4");
-		h4.className = "h4"
-		h4.append(document.createTextNode("Now please select the jigsaw block on the left and drag it to the correct position of the background."));
-		uploadEx.append(h4);
 }
 
 function setDefaultImgs(){
@@ -51,4 +55,35 @@ function setDefaultImgs(){
 	defaultImg.push(imga.src);
 	defaultImg.push(imgb.src);
 	defaultImg.push(imgc.src);
+}
+
+function resetDefault(){
+
+	canvas2.height = canvas2.height;
+	block2.height = block2.height;
+	//const canvas = canvas2.getContext("2d");
+	//canvas.clearRect(0,0,320, 200);
+	//const block = block2.getContext("2d");
+	//block.clearRect(0,0,320, 200);
+	ex2.pastX = 0;
+	ex2.makeJigsaw(0, 0, 320, 200, 50, "src");
+
+}
+
+function resetUpload(){
+
+	if(uploadedImg.length === 0){
+		alert("Please upload images first!");
+	}
+	else{
+		//const canvas1 = canvas.getContext("2d");
+		//canvas1.clearRect(0,0,320, 200);
+		canvas.height = canvas.height;
+		block.height = block.height;
+		//const block1 = block.getContext("2d");
+		//block1.clearRect(0,0,320, 200);
+
+		ex1.pastX = 0;
+		ex1.makeJigsaw(0, 0, 320, 200, 50, "file");
+	}
 }
