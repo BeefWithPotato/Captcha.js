0a5859d64282636eea3ca548706a4939ca1db13e
Haosheng Zhang <haoshengg.zhang@mail.utoronto.ca>
2020-08-13 00:19:37 -0400

    adding vertical slide jigsaw by peer review

--- public/js/examples.js
+++ public/js/examples.js
@@ -1,6 +1,7 @@
 'use strict';
 const log = console.log
 
+//by user
 const uploadedImg = [];
 const ex1 = new SlideStyleGenerator();
 ex1.imgs = uploadedImg;
@@ -10,24 +11,30 @@ const block = ex1.createCanvas(320, 200, "part1", "jigsaw");
 uploadEx.append(canvas);
 uploadEx.append(block);
 
+//default
+// const defaultImg = [];
+// setDefaultImgs();
+// const ex2 = new SlideStyleGenerator();
+// ex2.srcs = defaultImg;
+// const defaultEx = document.querySelector('#by-developer');
+// const canvas2 = ex2.createCanvas(320, 200, "canvas2", "background");
+// const block2 = ex2.createCanvas(320, 200, "part2", "jigsaw");
+// defaultEx.append(canvas2);
+// defaultEx.append(block2);
+// ex2.makeJigsaw(0, 0, 320, 200, 50, "src");
 
-const defaultImg = [];
-setDefaultImgs();
-const ex2 = new SlideStyleGenerator();
-ex2.srcs = defaultImg;
-const defaultEx = document.querySelector('#by-developer');
-const canvas2 = ex2.createCanvas(320, 200, "canvas2", "background");
-const block2 = ex2.createCanvas(320, 200, "part2", "jigsaw");
-
-defaultEx.append(canvas2);
-defaultEx.append(block2);
-ex2.makeJigsaw(0, 0, 320, 200, 50, "src");
-// const button = document.createElement("button");
-// button.type = "button";
-// button.onclick = resetDefault();
-// button.append(document.createTextNode("Reset"));
-// defaultEx.append(button);
-
+//Text style
+// const ex3 = new TextEnterGenerator();
+// const textEx = document.querySelector('#text');
+// const canvas = ex3.createCanvas(300, 120, "canvas");
+// const canvasA = ex3.createCanvas(300, 120, "canvasA");
+// ex3.canvasA = canvasA;
+// textEx.append(canvas);
+// textEx.append(canvasA);
+// ex3.createCode(canvas, 4);
+// ex3.drawRandomLines(canvas, 20, 1);
+// ex3.addDots(20, 2, 1);
+// ex3.drawRandomDotsWithAnimation(canvasA);
 
 function saveImages(){
 		log("saveImage");
@@ -61,10 +68,6 @@ function resetDefault(){
 
 	canvas2.height = canvas2.height;
 	block2.height = block2.height;
-	//const canvas = canvas2.getContext("2d");
-	//canvas.clearRect(0,0,320, 200);
-	//const block = block2.getContext("2d");
-	//block.clearRect(0,0,320, 200);
 	ex2.pastX = 0;
 	ex2.makeJigsaw(0, 0, 320, 200, 50, "src");
 
@@ -76,12 +79,9 @@ function resetUpload(){
 		alert("Please upload images first!");
 	}
 	else{
-		//const canvas1 = canvas.getContext("2d");
-		//canvas1.clearRect(0,0,320, 200);
+		
 		canvas.height = canvas.height;
 		block.height = block.height;
-		//const block1 = block.getContext("2d");
-		//block1.clearRect(0,0,320, 200);
 
 		ex1.pastX = 0;
 		ex1.makeJigsaw(0, 0, 320, 200, 50, "file");
--- public/js/verification_code_creator.js
+++ public/js/verification_code_creator.js
@@ -10,9 +10,12 @@ function SlideStyleGenerator(){
 	this.imgs = [];
 	this.length = "";
 	this.pastX = 0;
+	this.pastY = "";
+	this.trailX = [];
 	this.trailY = [];
 	this.srcs = [];
 	this.curr_src = "";
+	this.verticalOrHorizontal = "";
 	this.imgData="";
 }
 
@@ -34,7 +37,6 @@ SlideStyleGenerator.prototype = {
 		}
 		else if(type === "jigsaw"){
 			this.part = canvas;
-			//canvas.style = "display: none";
 			canvas.className = name;
 		}
 		
@@ -46,11 +48,19 @@ SlideStyleGenerator.prototype = {
 		return Math.round(Math.random() * x);
 	},
 
+	verticalOrHorizontal: function(){
+    	const result = ["vertical", "horizontal"];
+		return result[Math.floor(Math.random() * 2)];
+    },
+
 	makeJigsaw: function(x, y, width, height, length, type){
 		
 		log("drawImageOnCanvas");
-		const partContext = this.part.getContext("2d");
 
+		const direction = ["vertical", "horizontal"][Math.floor(Math.random() * 2)];
+		console.log(direction)
+
+		const partContext = this.part.getContext("2d");
 		if(type === "file"){
 			const file = this.imgs[this.getRandomNumber(this.imgs.length-1)];
 			this.curr_file = file;
@@ -61,19 +71,34 @@ SlideStyleGenerator.prototype = {
 				const img = new Image();
 				img.onload = () => {
 					
-					const partX = this.getRandomNumber(this.canvasWidth / 2) + (this.canvasWidth / 2) - length;
-					const partY = this.getRandomNumber(this.canvasHeight / 2) + (this.canvasHeight / 2) - length;
-
+					let partX;
+					let partY;
+					if(direction === "horizontal"){
+						this.verticalOrHorizontal = "horizontal";
+						partX = this.getRandomNumber(this.canvasWidth / 2) + (this.canvasWidth / 2) - length;
+						partY = this.getRandomNumber(this.canvasHeight / 2) + (this.canvasHeight / 2) - length;
+					}
+					else{
+						this.verticalOrHorizontal = "vertical";
+						partX = this.getRandomNumber(this.canvasWidth) - length;
+						partY = this.getRandomNumber(this.canvasHeight / 2);
+						if(partY + this.length > this.canvasHeight / 2){
+							partY = this.canvasHeight / 2 - this.length;
+						}
+						if(partX < 0){
+							partX = 0;
+						}
+					}
 					this.x = partX;
 					this.y = partY;
 					this.length = length;
-					
+							
 					partContext.rect(partX, partY, length, length);
 					partContext.clip();
 					partContext.drawImage(img, x, y, width, height);
 					partContext.strokeStyle = "#FFFFF";
 					partContext.stroke();
-				};
+				}
 	  			img.src = event.target.result;
 			}
 			this.createBackground(x, y, width, height, type);
@@ -98,7 +123,7 @@ SlideStyleGenerator.prototype = {
 				partContext.strokeStyle = "#FFFFF";
 				partContext.stroke();
 				this.imgData = partContext.getImageData(this.x, this.y, this.length, this.length);
-			};
+			}
 	  		img.src = src;
 			
 			this.createBackground(x, y, width, height, type);
@@ -121,19 +146,31 @@ SlideStyleGenerator.prototype = {
 				img.onload = () => {
 					canvasContext.drawImage(img, x, y, width, height);
 					canvasContext.clearRect(this.x, this.y, this.length, this.length);
-					try{
-						const imgData = partContext.getImageData(this.x, this.y, this.length, this.length);
-						partContext.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
-						partContext.putImageData(imgData, 0, this.y);
-						//partContext.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+					const imgData = partContext.getImageData(this.x, this.y, this.length, this.length);
+					this.imgData = imgData;
+					partContext.clearRect(0, 0, this.canvasWidth, this.canvasHeight);
+
+					if(this.verticalOrHorizontal === "horizontal"){
+						try{
+							partContext.putImageData(imgData, 0, this.y);
+						}
+						catch(e){
+							this.createBackground(this.x, this.y, this.canvasWidth, this.canvasHeight, "file");
+						}
 					}
-					catch(e){
-						this.createBackground(this.x, this.y, this.canvasWidth, this.canvasHeight, "file");
+					else{
+						try{
+							partContext.putImageData(imgData, this.x, this.canvasHeight - this.length);
+						}
+						catch(e){
+							this.createBackground(this.x, this.y, this.canvasWidth, this.canvasHeight, "file");
+						}
 					}
-				};
+				}
 	  			img.src = event.target.result;
 			}
 			this.initMouseEvent(this.part, type);
+			
 		}
 		else if(type === "src"){
 
@@ -154,7 +191,7 @@ SlideStyleGenerator.prototype = {
 					this.createBackground(this.x, this.y, this.canvasWidth, this.canvasHeight, "src");
 				}
 
-			};
+			}
 	  		img.src = this.curr_src;
 			
 			this.initMouseEvent(this.part, type);
@@ -162,108 +199,406 @@ SlideStyleGenerator.prototype = {
 
 	},
 
+	checkStandardDeviation: function(dataList){
+		const sum = dataList.reduce((a, b) => {
+			return a + b;
+		});
+
+		const average = sum / dataList.length;
+
+		const deviations = dataList.map((x) => {
+				return (x - average) ^ 2;
+		});
+
+		const sumD = deviations.reduce((a, b) => {
+			return a + b;
+		});
+
+		const standardDeviation = Math.sqrt(sumD / dataList.length);
+
+		if(standardDeviation !== 0){
+			return true;
+		}
+		else{
+			return false;
+		}
+	},
+
 	initMouseEvent: function(canvas, type){
 
 		canvas.onmousedown = (e) => {
 			const partContext = canvas.getContext("2d");
-			//const backgroundContext = background.
-			let imgData = partContext.getImageData(this.pastX, this.y, this.length, this.length);
-			//log(e.clientX)
-
+			let imgData;
 			canvas.onmousemove = (e) => {
-				let x = e.clientX;
-				const y = e.clientY;
-
-				//records y-axis changes when moving and use the data to verify if user is a machine
-				this.trailY.push(y);
-
-				//clear the canvas (I don't know why clearRect() doesn't work here, so I change the height of
-				//the canvas to force it to clear.)
-				canvas.height = canvas.height;
 				
-				//x can only move in the canvas so does jigsaw part
-				if(x + 25 > this.canvasWidth){
-					x = this.canvasWidth - 25;
+				if(this.verticalOrHorizontal === "horizontal"){
+					let x = e.clientX;
+					const y = e.clientY;
+					imgData = this.imgData;
+					//imgData = partContext.getImageData(this.pastX, this.y, this.length, this.length);
+					//records y-axis changes when moving and use the data to verify if user is a machine
+					this.trailY.push(y);
+
+					//clear the canvas (I don't know why clearRect() doesn't work here, so I change the height of
+					//the canvas to force it to clear.)
+					canvas.height = canvas.height;
+							
+					//jigsaw can only move in the canvas
+					if(x + 25 > this.canvasWidth){
+						x = this.canvasWidth - 25;
+					}
+					else if(x - 25 < 0){
+						x = 0 + 25;
+					}
+					console.log(x)
+					console.log(y)
+					partContext.putImageData(imgData, x-25, this.y-1);
+			        this.pastX = x-25;
+			        //imgData = partContext.getImageData(this.pastX, this.y, this.length, this.length);
 				}
-				else if(x - 25 < 0){
-					x = 0 + 25;
-				}
+			        
+			    else if(this.verticalOrHorizontal === "vertical"){
+			    	const x = e.clientX;
+			    	let y = e.clientY;
+			    	imgData = this.imgData;
+			    	//imgData = partContext.getImageData(this.x, this.canvasHeight-this.length, this.length, this.length);
+			        //records y-axis changes when moving and use the data to verify if user is a machine
+					this.trailX.push(x);
 
-            	partContext.putImageData(imgData, x-25, this.y);
-            	this.pastX = x-25;
-            	//this.verifyResult(this.pastX, canvas);
-            	imgData = partContext.getImageData(this.pastX, this.y, this.length, this.length);
+					//clear the canvas (I don't know why clearRect() doesn't work here, so I change the height of
+					//the canvas to force it to clear.)
+					canvas.height = canvas.height;
+							
+					// jigsaw can only move in the canvas
+					// if(y - 200 + 25 > this.canvasHeight){
+					// 	y = this.canvasHeight - 25;
+					// }
+					// else if(y - 25 - 200 < 0){
+					// 	y = 0;
+					// }
+			        partContext.putImageData(imgData, this.x, y-200);
+			    }	
+			}
+			canvas.onmouseup = (e) => {
+				canvas.onmousemove = null;
+			    canvas.onmouseup = null;
 
-            	canvas.onmouseup = (e) => {
-					canvas.onmousemove = null;
-	            	canvas.onmouseup = null;
-					if(Math.abs(e.clientX - 25 - this.x) <= 10){
-
-						const sum = this.trailY.reduce((a, b) => {
-						    return a + b;
-						});
-
-						const average = sum / this.trailY.length;
-
-						const deviations = this.trailY.map((x) => {
-							return (x - average) ^ 2;
-						});
-
-						const sumD = deviations.reduce((a, b) => {
-						    return a + b;
-						});
-
-						const standardDeviation = Math.sqrt(sumD / (this.trailY.length - 1));
-						log(standardDeviation);
-
-						if(standardDeviation !== 0){
-							alert("Success");
-							canvas.onmousemove = null;
-			            	canvas.onmouseup = null;
-			            	canvas.onmousedown = null;
-							return true;
+				if(this.verticalOrHorizontal === "horizontal"){
+					if(Math.abs(e.clientX - 25 - this.x) <= 5){
+						if(this.checkStandardDeviation(this.trailY)){
+							alert("Success!");
 						}
 						else{
-							alert("Failed");
-							canvas.onmousemove = null;
-			            	canvas.onmouseup = null;
-			            	canvas.onmousedown = null;
-							return false;
+							alert("You are a bot!");
 						}
-
 					}
 					else{
 						alert("Failed. Please try again!");
 						canvas.onmousemove = null;
-			            canvas.onmouseup = null;
-			            canvas.onmousedown = null;
+						canvas.onmouseup = null;
+						canvas.onmousedown = null;
 
-			            const background = this.canvas.getContext("2d");
+						const background = this.canvas.getContext("2d");
 						background.clearRect(0,0,this.canvasWidth, this.canvasHeight);
 						const part = canvas.getContext("2d");
 						part.clearRect(0,0,this.canvasWidth, this.canvasHeight);
 
-						this.pastX = 0;
+						
 						if(type === "file"){
 							this.makeJigsaw(0, 0, 320, 200, 50, "file");
 						}
 						else if(type === "src"){
 							this.makeJigsaw(0, 0, 320, 200, 50, "src");
-						}
-							
+						}			
 					}
-				};
-			};
+				}
+				if(this.verticalOrHorizontal === "vertical"){
+					if(Math.abs(e.clientY - 200 - this.y) <= 2){
+						if(this.checkStandardDeviation(this.trailX)){
+							alert("Success!");
+						}
+						else{
+							alert("You are a bot!");
+						}
+					}
+					else{
+						alert("Failed. Please try again!");
+						canvas.onmousemove = null;
+						canvas.onmouseup = null;
+						canvas.onmousedown = null;
+
+						const background = this.canvas.getContext("2d");
+						background.clearRect(0,0,this.canvasWidth, this.canvasHeight);
+						const part = canvas.getContext("2d");
+						part.clearRect(0,0,this.canvasWidth, this.canvasHeight);
+
+						
+						if(type === "file"){
+							this.makeJigsaw(0, 0, 320, 200, 50, "file");
+						}
+						else if(type === "src"){
+							this.makeJigsaw(0, 0, 320, 200, 50, "src");
+						}			
+					}
+				}
+				
+			}
+		}
+	},
+
+};
+
+function Dot(){
+	this.x = "";
+	this.y = "";
+	this.radius = "";
+	this.color = "";
+	this.speedX = "";
+	this.speedY = "";
+};
+
+window.requestAnimFrame = (function(){
+  	return  window.requestAnimationFrame   ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame    ||
+        function( callback ){
+            window.setTimeout(callback, 1000 / 60);
+        };
+})();
+
+function TextEnterGenerator(){
+	this.nums = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+	this.letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+	this.font = "40pt Calibri";
+	this.angle = 15;
+	this.canvas = "";
+	this.canvasWidth = "";
+	this.canvasHeight = "";
+	this.dots = [];
+	this.canvasA = "";
+	this.dotSpeed = "";
+}
+
+TextEnterGenerator.prototype = {
+
+	createCanvas: function(width, height, name){
+
+		const canvas = document.createElement("canvas");
+		canvas.width = width;
+		this.canvasWidth = width;
+		canvas.height = height;
+		this.canvasHeight = height;
+		canvas.className = name;
+		canvas.style = "border: 1px solid black";
+		return canvas;
+	},
+
+	setFont: function(font){
+		this.font = font;
+	},
+
+	setAngle: function(angle){
+		this.angle = angle;
+	},
+
+	boldOrNot: function(){
+		const result = ["bold", "none"];
+		return result[Math.round(Math.random())];
+	},
+
+	getRandomNumber: function(){
+		return this.nums[Math.floor(Math.random() * 10)];
+	},
+
+	upperOrLower: function(){
+		const result = ["lower", "upper"];
+		return result[Math.round(Math.random())];
+	},
+
+	getRandomSpeed: function(x){
+		return Math.round(Math.random() * x);
+	},
+
+	getRandomLetter: function(){
+		if(this.upperOrLower() === "lower"){
+			const index = Math.floor(Math.random() * 26);
+			const result = this.letters[index];
+			return result;
+		}
+		else{
+			const index = Math.floor(Math.random() * 26);
+			const result = this.letters[index].toUpperCase();
+			return result;
+		}
+	},
+
+	getRandomColor: function(){
+        const r = Math.round(Math.random() * 256);
+        const g = Math.round(Math.random() * 256);
+        const b = Math.round(Math.random() * 256);
+        return "rgb(" + r + "," + g + "," + b +")";
+    },
+
+    numberOrLetter: function(){
+    	const result = ["number", "letter"];
+		return result[Math.floor(Math.random() * 2)];
+    },
+
+    getRandomAngle: function(){
+    	const degree = Math.round(Math.random() * this.angle);
+    	return degree * Math.PI / 180;
+    },
+
+    negOrPos: function(){
+    	const result = ["positive", "negative"];
+    	return result[Math.floor(Math.random() * 2)];
+    },
+
+    strokeOrfill: function(){
+    	const result = ["stroke", "fill"];
+    	return result[Math.floor(Math.random() * 2)];
+    },
+
+    addDots: function(num, radius, speed){
+    	let i;
+    	for(i = 0; i < num; i++){
+    		const newDot = new Dot();
+    		this.dotSpeed = speed;
+    		newDot.x = Math.round(Math.random() * this.canvasWidth);
+    		newDot.y = Math.round(Math.random() * this.canvasHeight);
+    		newDot.radius = radius;
+    		newDot.speedX = this.getRandomSpeed(speed);
+    		newDot.speedY = this.getRandomSpeed(speed);
+    		newDot.color = this.getRandomColor();
+    		this.dots.push(newDot);
+    	}
+    },
+
+    drawRandomDots: function(canvas){
+
+    	const canvasContext = canvas.getContext("2d");
+    	let i;
+	    for(i = 0; i < this.dots.length; i++){
+			canvasContext.beginPath();
+			canvasContext.arc(this.dots[i].x, this.dots[i].y, this.dots[i].radius, 0, 2 * Math.PI);
+				
+			if(this.strokeOrfill() === "fill"){
+				canvasContext.fillStyle = this.dots[i].color;
+				canvasContext.fill();
+			}
+			else{
+				canvasContext.strokeStyle = this.dots[i].color;
+				canvasContext.stroke(); 
+			}
+			canvasContext.closePath();
+	    }
+    },
+
+    moveDots: function(){
+    	let i;
+    	for(i = 0; i < this.dots.length; i++){
+
+    		if(this.dots[i].speedX === 0 && this.dots[i].speedY === 0){
+    			this.dots[i].speedX = this.getRandomSpeed(this.dotSpeed);
+    			this.dots[i].speedY = this.getRandomSpeed(this.dotSpeed);
+    		}
+
+	    	if(this.dots[i].x < 0 ){
+	    		this.dots[i].speedX = -this.dots[i].speedX;
+	    	}
+	    	if(this.dots[i].x > this.canvasWidth){
+	    		this.dots[i].speedX = -this.dots[i].speedX;
+	    	}
+	    	if(this.dots[i].y < 0){
+	    		this.dots[i].speedY = -this.dots[i].speedY;
+	    	}
+	    	if(this.dots[i].y > this.canvasHeight){
+	    		this.dots[i].speedY = -this.dots[i].speedY;
+	    	}
+	    	this.dots[i].x += this.dots[i].speedX;
+	    	this.dots[i].y += this.dots[i].speedY;
+	    }
+    	
+    },
+
+    drawRandomDotsWithAnimation: function(canvas){
+
+    	this.moveDots();
+    	canvas.height = canvas.height;
+    	this.drawRandomDots(canvas, this.dots.length, 2);
+
+	    
+	    requestAnimFrame(() => {
+	    	this.drawRandomDotsWithAnimation(canvas);
+	    })
+
+    },
+
+    drawRandomLines: function(canvas, num, width){
+    	const canvasContext = canvas.getContext("2d");
+    	for(let i = 0; i < num; i++){
+			const startX = Math.round(Math.random() * this.canvasWidth);
+			const startY = Math.round(Math.random() * this.canvasHeight);
+			const endX = Math.round(Math.random() * this.canvasWidth);
+			const endY = Math.round(Math.random() * this.canvasHeight);
+
+			canvasContext.beginPath();
+			canvasContext.moveTo(startX, startY);
+			canvasContext.lineWidth = width;
+			canvasContext.lineTo(endX,endY); 
+			canvasContext.strokeStyle = this.getRandomColor();
+			canvasContext.stroke();
+			canvasContext.closePath();
+		}
+
+    },
+
+	createCode: function(canvas, digits){
+		const canvasContext = canvas.getContext("2d");
+		const code = [];
+		let i;
+		for(i = 0; i < digits; i++){
+			if(this.numberOrLetter() === "number"){
+				code.push(this.getRandomNumber());
+			}
+			else{
+				code.push(this.getRandomLetter());
+			}
+		}
+
+		let pos = 0;
+		for(i = 0; i < digits; i++){
+
+			if(this.boldOrNot === "bold"){
+				this.font = "bold" + this.font;
+			}
+
+			const x = this.canvasWidth / (digits + 2);
+			const y = this.canvasHeight / 2;
+			canvasContext.font = this.font;
+			canvasContext.textBaseline = "middle";
+
+			let angle;
+			if(this.negOrPos() === "positive"){
+				angle = this.getRandomAngle();
+				canvasContext.rotate(angle);
+			}
+			else{
+				angle = -this.getRandomAngle();
+				canvasContext.rotate(angle);
+			}
+
+			if(this.strokeOrfill() === "fill"){
+				canvasContext.fillStyle = this.getRandomColor();
+				canvasContext.fillText(code[i], x * (i+1), y);
+			}
+			else{
+				canvasContext.strokeStyle = this.getRandomColor();
+				canvasContext.strokeText(code[i], x * (i+1), y);
+			}
+			canvasContext.rotate(-angle);
 		}
 	},
 
 
-};
-
-// var ex = new SlideStyleGenerator();
-// const body = document.querySelector('body');
-// const canvas = ex.createCanvas(320, 200, "canvas");
-// const block = ex.createCanvas(320, 200, "part");
-//body.append(ex.createInputBox());
-// body.append(canvas);
-// body.append(block);
+}
